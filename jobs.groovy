5d66da9744e94c64f9a0cf5e20a4c111037ff419
konstantinskorin <52405326+konstantinskorin@users.noreply.github.com>
2019-07-25 16:35:54 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,15 +1,9 @@
-
-def jobList = []
-def firstJobIndex = 1
-def lastJobIndex = 4
 def command = "git ls-remote -h https://github.com/konstantinskorin/d323dsl.git"
+def selectedBranches = command.execute().text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')}
+  selectedBranches.removeAll {!(["master","kskorin"].contains(it)) }
 
 job("MNTLAB-kskorin-main-build-job") {
   
-def selectedBranches = command.execute().text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')}
-  selectedBranches.removeAll {!(["master","kskorin"].contains(it)) }
-  
-  for(i=firstJobIndex; i<lastJobIndex+1; i++){
     
     parameters {
       choiceParam('BRANCH_NAME',  selectedBranches,'')
@@ -17,30 +11,15 @@ def selectedBranches = command.execute().text.readLines().collect {it.split()[1]
             description('Choose: ')
             choiceType('CHECKBOX')
             groovyScript {
-            script('["MNTLAB-kskorin-child${i}-build-job"]')
+                script('''list = []
+				for(i in 1..4){
+  				list.add("MNTLAB-kskorin-child${i}-build-job")
+    			}
+				return list''')
+            	}
+   		}
     }
-
-    //create downstream job
-    job("MNTLAB-kskorin-child${i}-build-job") {
-      scm {
-        github('konstantinskorin/d323dsl', 'kskorin')
-      }
-      def allBranches = command.execute().text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')}
-      allBranches.remove(kskorin)
-      allBranches.add(0,kskorin)
-      parameters {
-        choiceParam('BRANCH_NAME',  allBranches,'')
-      }
-      steps {
-      shell('''bash script.sh > output.txt
-	       tar -czf ${BRANCH_NAME}_dsl_script.tar.gz script.sh jobs.groovy''')
-      }
-      publishers {
-       			archiveArtifacts '${BRANCH_NAME}_dsl_script.tar.gz, output.txt'
-     }
-    }
-    jobList << "MNTLAB-kskorin-child${i}-build-job"
-    steps {
+  steps {
         downstreamParameterized {
                 trigger('$BUILDS_TRIGGER') {
                         block{
@@ -50,10 +29,29 @@ def selectedBranches = command.execute().text.readLines().collect {it.split()[1]
                         }
                         parameters {
                                 predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
-                        }
+                                                }
                 }
         }    
     }
-  }
 }
-
+for(i in 1..4){   
+    //create downstream job
+    job("MNTLAB-kskorin-child${i}-build-job") {
+      scm {
+        github('konstantinskorin/d323dsl', 'kskorin')
+      }
+      def allBranches = command.execute().text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')}
+      allBranches.remove("kskorin")
+      allBranches.add(0,"kskorin")
+      parameters {
+        choiceParam('BRANCH_NAME',  allBranches,'')
+      }    
+      steps {
+      shell('''bash script.sh > output.txt
+	       tar -czf ${BRANCH_NAME}_dsl_script.tar.gz script.sh jobs.groovy''')
+      }
+      publishers {
+       			archiveArtifacts '${BRANCH_NAME}_dsl_script.tar.gz, output.txt'
+     }
+    }
+}
